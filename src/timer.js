c6d04cf6fb90d4a01109b199dd5076aa23ec91b1
宋均均 <sjj107294@alibaba-inc.com>
2020-03-13 22:33:47 +0800

    feat: 优化

--- src/timer.js
+++ src/timer.js
@@ -1,32 +1,34 @@
 /**
- * 节流(被调用n次,只在time间隔时间点运行)
+ * 防抖(被调用n次,只运行一次)
  * @param fn 回调函数
- * @param time 延时
+ * @param timeout 延时
  */
-function throttle (fn, time = 300) {
-  if (fn.pending) {
-    return;
-  }
-
-  fn();
-  fn.pending = true;
-
-  setTimeout(() => {
-    fn.pending = false;
-  }, time);
+function debounce (fn, timeout = 300) {
+  return function (...args) {
+    clearTimeout(fn._tid);
+    fn._tid = setTimeout(() => {
+      fn.call(this, ...args);
+    }, timeout);
+  };
 }
 
 /**
- * 防抖(被调用n次,只运行一次)
+ * 节流(被调用n次,只在time间隔时间点运行)
  * @param fn 回调函数
- * @param time 延时
+ * @param timeout 延时
  */
-function debounce (fn, time = 300) {
-  clearTimeout(fn.tid);
-  fn.tid = setTimeout(fn, time);
+function throttle (fn, timeout = 300) {
+  let start = Date.now();
+  return function (...args) {
+    const now = Date.now();
+    if (now - start >= timeout) {
+      fn.call(this, ...args);
+      start = now;
+    }
+  };
 }
 
 export {
-  throttle,
   debounce,
+  throttle,
 };
