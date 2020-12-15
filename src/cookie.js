f14786636cb1225edff8442bffff4284b0daf76c
宋均均 <songjunjun@bytedance.com>
2020-12-15 20:18:49 +0800

    feat: 支持path参数

--- src/cookie.js
+++ src/cookie.js
@@ -1,33 +1,28 @@
-const { toString } = {};
+const { toString } = {}
 
 /**
  * 设置cookie
  * @param {string} key 键
  * @param {string} val 值
  * @param {Date|number} days 过期时间|过期天数
- * @param {number} hours 过期小时数
+ * @param {string} path cookie的path
  */
-function setItem (key, val, days, hours) {
-  let str = `${key}=${encodeURIComponent(val)}`;
+function setItem(key, val, days, path = '/') {
+  let str = `${key}=${encodeURIComponent(val)}`
 
-  let expire;
-  // days参数是一个日期
+  let expire
+  // 过期时间
   if (toString.call(days) === '[object Date]') {
-    expire = days;
+    expire = days
   }
   // 过期天数
   else if (typeof days === 'number') {
-    expire = new Date();
-    expire.setDate(expire.getDate() + days);
-  }
-  // 过期小时数
-  else if (typeof hours === 'number') {
-    expire = new Date();
-    expire.setHours(expire.getHours() + hours);
+    expire = new Date()
+    expire.setDate(expire.getDate() + days)
   }
 
-  expire && (str += `;expires=${expire.toUTCString()}`);
-  document.cookie = str;
+  expire && (str += `;path=${path};expires=${expire.toUTCString()}`)
+  document.cookie = str
 }
 
 /**
@@ -35,17 +30,17 @@ function setItem (key, val, days, hours) {
  * @param {string} key 键
  * @returns {string} cookie值
  */
-function getItem (key) {
-  const { cookie } = document;
-  const index = cookie.indexOf(`${key}=`);
+function getItem(key) {
+  const { cookie } = document
+  const index = cookie.indexOf(`${key}=`)
 
   if (index !== -1) {
-    const start = index + key.length + 1;
-    let end = cookie.indexOf(';', start);
+    const start = index + key.length + 1
+    let end = cookie.indexOf(';', start)
 
     // 最后一个
-    end === -1 && (end = cookie.length);
-    return decodeURIComponent(cookie.slice(start, end));
+    end === -1 && (end = cookie.length)
+    return decodeURIComponent(cookie.slice(start, end))
   }
 }
 
@@ -53,12 +48,12 @@ function getItem (key) {
  * 清除cookie
  * @param {string} key 键
  */
-function removeItem (key) {
-  document.cookie = `${key}=_;expires=${new Date().toUTCString()}`;
+function removeItem(key) {
+  document.cookie = `${key}=_;expires=${new Date().toUTCString()}`
 }
 
 export default {
   setItem,
   getItem,
   removeItem,
-};
+}
